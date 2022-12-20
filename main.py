2675a3518b6727703def6b081530a01109cedb86
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-12-21 02:53:24 +0800

    Completed code

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# TODO: Find a way to redirect /add to /edit
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 import requests
+import os
 
-TMDB_API_KEY = '4ac04cb8e2c92ee08e07492596eb76a2'
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-movies-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    """Headings for SQLAlchemy database"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(500), nullable=False)
+
+
+db.create_all()
 
 
 class UpdateMovie(FlaskForm):
@@ -26,33 +46,11 @@ class AddMovie(FlaskForm):
     submit = SubmitField('Add Movie')
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-movies-collection.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.app_context().push()
-db = SQLAlchemy(app)
-
-
-class Movie(db.Model):
-    """Headings for SQLAlchemy database"""
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.String(250), nullable=False)
-    img_url = db.Column(db.String(500), nullable=False)
-
-    def __repr__(self):
-        return f'Movie: {self.title}'
-
-
-db.create_all()
-
-
 @app.route("/")
 def home():
-    all_movies = db.session.query(Movie).all()
+    all_movies = Movie.query.order_by(Movie.rating).all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
     return render_template('index.html', movies=all_movies)
 
 
@@ -105,14 +103,11 @@ def find():
         title=movie['title'],
         year=int(movie['release_date'].split('-')[0]),
         description=movie['overview'],
-        rating=round(movie['vote_average'], 1),
-        ranking=1,
-        review="Click on 'Update' to add review",
         img_url=f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
     )
     db.session.add(new_movie)
     db.session.commit()
-    return redirect(url_for('edit'))
+    return redirect(url_for('edit', id=new_movie.id))
 
 
 if __name__ == '__main__':
